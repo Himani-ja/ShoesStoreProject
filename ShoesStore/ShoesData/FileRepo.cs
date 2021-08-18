b8ebcd21a42bf8989f5917b8d29512f3549a0e41
Himani-ja <himani.jain2309@gmail.com>
2021-08-18 12:52:27 +0530

    fixed merge conflicts

---
+++ ShoesStore/ShoesData/FileRepo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+
+namespace ShoesData
+{
+    public class FileRepo
+    {
+        static List<shoes> cats = null;
+        static string path = @"C:\shoesproject\ShoesStoreProject\ShoesStore\ShoesData\shoes.xml";
+        public List<shoes> Init(int id,Category category,double size,double price,Colors color,Types type,Lace lace,string brand,int quantity)
+        {
+            cats = new List<shoes>(){
+                    new shoes(){Id=id,Category=category,Size=size,Price=price,Color=color,Type=type,Lace=lace,Brand=brand,Quantity=quantity},
+                    
+                };
+            return cats;
+        }
+        public void Addshoes(List<shoes> cats)
+        {
+            StreamWriter writer = new StreamWriter(path);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<shoes>));
+            serializer.Serialize(writer, cats);
+
+            writer.Close();
+            System.Console.WriteLine("All cats has bee stored in the XML file at {0}", path);
+        }
+
+
+    }
+}
